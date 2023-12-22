55a87800e633fcdda01e72d694aa537d26dfd130
ysh4338 <ec2-user@ip-10-0-0-245.ap-northeast-2.compute.internal>
2023-12-22 09:42:04 +0000

    change create-ec2-script to db-sub > pri-sub

--- scripts/create_ec2_instance.sh
+++ scripts/create_ec2_instance.sh
@@ -2,19 +2,18 @@
 
 # VPC / SUBNETS INFORMATION
 VPC_NAME="lab-edu-vpc-ap-01"
-PRI_SUB_NAME="lab-edu-sub-pri-01"
-DB_SUB_NAME="lab-edu-sub-db-01"
+PRI_SUB_NAME_01="lab-edu-sub-pri-01"
+PRI_SUB_NAME_02="lab-edu-sub-pri-02"
 NETWORK_EC2_NAME_01="lab-edu-ec2-network-ap-01"
 NETWORK_EC2_NAME_02="lab-edu-ec2-network-ap-02"
 
 VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=$VPC_NAME" --query "Vpcs[].VpcId" --output text)
-SUBNET_ID_PRIVATE=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=$PRI_SUB_NAME" --query "Subnets[].SubnetId" --output text)
-SUBNET_ID_DB=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=$DB_SUB_NAME" --query "Subnets[].SubnetId" --output text)
+SUBNET_ID_PRIVATE_01=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=$PRI_SUB_NAME_01" --query "Subnets[].SubnetId" --output text)
+SUBNET_ID_PRIVATE_02=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=$PRI_SUB_NAME_02" --query "Subnets[].SubnetId" --output text)
 
 # INSTANCE INFORMATION
 AMI_ID="ami-0ff1cd0b5d98708d1"
 INSTANCE_TYPE="t3.micro"
-
 
 # CREATE KEY-PAIR
 KEY_NAME="lab-edu-pem-network"
@@ -29,8 +28,11 @@ SG_ID=$(aws ec2 create-security-group --group-name $SG_NAME --description "My se
 aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 22 --cidr 10.0.0.0/8 > /dev/null
 
 # CREATE INSTANCES
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID_PRIVATE --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NETWORK_EC2_NAME_01}]" > /dev/null
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID_DB --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NETWORK_EC2_NAME_02}]" > /dev/null
+NETWORK_EC2_IP_01=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID_PRIVATE_01 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NETWORK_EC2_NAME_01}]" --query "Instances[0].PrivateIpAddress" --output text)
+NETWORK_EC2_IP_02=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID_PRIVATE_02 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NETWORK_EC2_NAME_02}]" --query "Instances[0].PrivateIpAddress" --output text)
+
+echo "$NETWORK_EC2_IP_01 | $NETWORK_EC2_NAME_01" >> ec2_information.txt
+echo "$NETWORK_EC2_IP_02 | $NETWORK_EC2_NAME_02" >> ec2_information.txt
 
 
 
