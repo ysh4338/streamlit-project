93287da7033b381cd3df8f7006306d22c2f22b5f
ysh4338 <ysh4338@naver.com>
2023-12-22 23:36:42 +0000

    update cloudformation network baseline

--- scripts/upload_cloudformation_template_to_s3.sh
+++ scripts/upload_cloudformation_template_to_s3.sh
@@ -5,7 +5,7 @@ TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X
 ACCOUNT_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document --header "X-aws-ec2-metadata-token: $TOKEN" | jq -r .accountId)
 
 BUCKET_NAME="lab-edu-bucket-cloudformation-$ACCOUNT_ID"
-FOLDER_PATH="../infra_as_a_code"
+FOLDER_PATH="streamlit-project/infra_as_a_code/"
 
 aws s3 mb "s3://$BUCKET_NAME"
 aws s3 cp "$FOLDER_PATH" "s3://$BUCKET_NAME" --recursive
