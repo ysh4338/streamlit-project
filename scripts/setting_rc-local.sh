528621b63911127808316c49964dd31032ee8c83
ysh4338 <ysh4338@naver.com>
2024-01-07 03:47:23 +0000

    update codedeploy

--- scripts/setting_rc-local.sh
+++ scripts/setting_rc-local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Configuration to restart streamlit application 
 touch /etc/rc.d/rc.local
 cat <<EOT >> /etc/rc.d/rc.local
 #!/bin/bash
@@ -8,9 +9,18 @@ EOT
 ln -s /etc/rc.d/rc.local /etc/rc.local
 chmod 755 /etc/rc.d/rc.local
 
+# Configuration System demon 
 echo " " >> /lib/systemd/system/rc-local.service 
 echo "[Install]" >> /lib/systemd/system/rc-local.service 
 echo "WantedBy=multi-user.target" >> /lib/systemd/system/rc-local.service 
 
+# Install Code-Deploy Agent
 systemctl daemon-reload
 systemctl enable rc-local
+
+yum install ruby -y
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+chmod +x install
+./install auto
+service codedeploy-agent status
